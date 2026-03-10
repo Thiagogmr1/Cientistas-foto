821cece62125d80999bef34551795907be0c38e1
Thiago Gabriel <thiagogab.20@gmail.com>
2026-03-10 17:55:26 -0300

    refactor: criação da pasta components

---
+++ src/components/Lens.jsx
@@ -0,0 +1,53 @@
+import { useState, useRef } from 'react'
+
+function Lens() {
+
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    const [showLens, setShowLens] = useState(false)
+    const imgRef = useRef(null)
+    
+    const handleMouseMove = (e) => {
+        const { left, top } = e.currentTarget.getBoundingClientRect()
+    
+        const x = e.clientX - left
+        const y = e.clientY - top
+    
+        setPosition({ x, y })
+    }
+
+    return (
+        <main>
+            <div
+                style={{ position: "relative" }}
+                onMouseMove={handleMouseMove}
+                onMouseEnter={() => setShowLens(true)}
+                onMouseLeave={() => setShowLens(false)}
+            >
+                <img ref={imgRef} src="/cientistas.jpg" alt="Cientistas" />
+
+                {showLens && (
+                    <div
+                        style={{
+                            position: "absolute",
+                            left: position.x - 75,
+                            top: position.y - 75,
+                            width: "150px",
+                            height: "150px",
+                            borderRadius: "50%",
+                            border: "2px solid black",
+                            pointerEvents: "none",
+                            backgroundImage: "url(/cientistas-color.jpg)",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "1000%",
+                            backgroundPosition: imgRef.current
+                                ? `${(position.x / imgRef.current.offsetWidth) * 100}% 
+                                ${(position.y / imgRef.current.offsetHeight) * 100}%`
+                                : "center"
+                        }}
+                    />
+                )}
+            </div>
+        </main>
+    )
+}
+export default Lens
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,16 @@
-import { useState, useRef } from 'react'
+import Lens from "../components/Lens"
 
 function Home() {
-
-    const [position, setPosition] = useState({ x: 0, y: 0 })
-    const [showLens, setShowLens] = useState(false)
-    const imgRef = useRef(null)
-    
-    const handleMouseMove = (e) => {
-        const { left, top } = e.currentTarget.getBoundingClientRect()
-    
-        const x = e.clientX - left
-        const y = e.clientY - top
-    
-        setPosition({ x, y })
-    }
-
     return (
-        <main>
-            <div
-                style={{ position: "relative" }}
-                onMouseMove={handleMouseMove}
-                onMouseEnter={() => setShowLens(true)}
-                onMouseLeave={() => setShowLens(false)}
-            >
-                <img ref={imgRef} src="/cientistas.jpg" alt="Cientistas" />
-
-                {showLens && (
-                    <div
-                        style={{
-                            position: "absolute",
-                            left: position.x - 75,
-                            top: position.y - 75,
-                            width: "150px",
-                            height: "150px",
-                            borderRadius: "50%",
-                            border: "2px solid black",
-                            pointerEvents: "none",
-                            backgroundImage: "url(/cientistas-color.jpg)",
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "1000%",
-                            backgroundPosition: imgRef.current
-                                ? `${(position.x / imgRef.current.offsetWidth) * 100}% 
-                                ${(position.y / imgRef.current.offsetHeight) * 100}%`
-                                : "center"
-                        }}
-                    />
-                )}
-            </div>
+        <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <Lens
+                bwImage="/cientistas-bw.jpg"
+                colorImage="/cientistas-color.jpg"
+                size={150}
+                zoom={3}
+            />
         </main>
     )
 }
+
 export default Home
